43b4253764fb0348ad286b410c805ca2a3082339
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2021-05-01 11:33:21 +0200

    refactor Application and create AdminCommandHandler file

---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/presentation/handler/AdminCommandHandler.java
@@ -0,0 +1,12 @@
+package com.epam.training.ticketservice.presentation.handler;
+
+import org.springframework.shell.standard.ShellComponent;
+
+/**
+ * Command handler for 'admin' command
+ */
+
+@ShellComponent
+public class AdminCommandHandler {
+
+}
